41a6658430980b1e9901c47e3cecc52b22ab790b
Tadeusz <tadeusz.potoniec@parhelium.com>
2023-01-25 08:13:41 +0100

    Initial Commit

---
+++ C++L13P13/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  C++L13P13
+//
+//  Created by Tadeusz Potoniec on 25/01/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
